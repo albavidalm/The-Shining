e3afdc27e38f5d3bde74eb75bc78aca251ff192d
Alba V <albavidalm@gmail.com>
2021-08-27 18:57:46 +0200

    Add button style

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+const slides = document.querySelector(".js-slides");
+const button = document.querySelectorAll(".js-button");
+
+button.forEach((eachButton, i) => {
+  button[i].addEventListener("click", () => {
+    let position = i;
+    let movement = position * -(100 / 7);
+
+    slides.style.transform = `translateX(${movement}%)`;
+
+    button.forEach((eachButton, i) => {
+      button[i].classList.remove("active");
+    });
+
+    button[i].classList.add("active");
+  });
+});
